7772998411ccb04e52a5c75a107a42c5b6ef42f8
Michael Sliwinski <michaelnozbe@users.noreply.github.com>
2020-07-21 14:20:28 +0200

    Fixed now 2

--- js/scripts.js
+++ js/scripts.js
@@ -5,22 +5,21 @@
 const URL = "https://michael.team/";
 
 function getNow () {
-	let intro = document.querySelector("#intro");
-	intro.after(createSpinner());
-	alert (URL);
-	let now = document.createElement('article');
-	now.innerHTML = getArticle('now200626');
-	createSpinner(true);
-	intro.after(now);
+  getArticle('now200626','#intro');
 }
 
-function getArticle (url) {
+function getArticle (url,where) {
+	let place = document.querySelector(where); //get place after which we'll put stuff
+	place.after(createSpinner()); //show spinner
+	let element = document.createElement('article'); //create article
 	fetch(URL+url)
 	.then((response) => response.text())
 	.then((responseText) => {
 		let parser = new DOMParser();
 		let fullHTML = parser.parseFromString (responseText, 'text/html');
-		return fullHTML.querySelector('article').innerHTML;
+		element.innerHTML = fullHTML.querySelector('article').innerHTML; //get article
+		createSpinner(true); //remove spinner
+		place.after(element); //put the article where it belongs
 	})
 	.catch((error) => {
 		console.error(error)
