fa35160806cdbe741df9eac967d511eb9de9890b
Michael Sliwinski <michaelnozbe@users.noreply.github.com>
2020-07-21 14:01:57 +0200

    Now 2 tests and introduction to scripts

--- en/now2.md
+++ en/now2.md
@@ -3,20 +3,13 @@ layout: default
 permalink: /now2/
 title: "Michael Sliwinski's NOW page"
 subtitle: "Semi-regular update on Nozbe and other productivity projects in my life"
-js: getnow
+js: getNow
 ---
 
 <div class="header headerblack">
 	<h1>{{ page.title }}</h1>
 	<h2>{{ page.subtitle }}</h2>
 </div>
-
-{::options parse_block_html="true" /}
-
 <div class="entry" id="intro">
-
-	This is a [“now”](https://nownownow.com/p/Wwy1) page inspired by [Derek Sievers](https://sivers.org/now). It's a "status update" on my life, my projects and priorities. I try to post a new update [every Friday](/tag/now/) while doing a weekly review.
-
-	Here's my latest update:
-
+	{{ site.data.main.now | markdownify }}
 </div>
---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+// scripts for michael.team
+// written by Michael Sliwinski
+// published under CC license: michael.team/license
+
+const URL = "https://michael.team/";
+
+function getNow () {
+	let intro = document.querySelector("#intro");
+	intro.after(createSpinner());
+	let now = document.createElement('article');
+	now.innerHTML = getArticle('now200626');
+	createSpinner(true);
+	intro.after(now);
+}
+
+function getArticle (url) {
+	fetch(URL+url)
+	.then((response) => response.text())
+	.then((responseText) => {
+		let parser = new DOMParser();
+		let fullHTML = parser.parseFromString (responseText, 'text/html');
+		return fullHTML.querySelector('article').innerHTML;
+	})
+	.catch((error) => {
+		console.error(error)
+	})
+}
+
+function createSpinner (destroy=false) {
+	let spinner = document.createElement('div');
+	spinner.setAttribute('id','spinner');
+	spinner.setAttribute('class','spinner');
+	if (destroy) {
+		document.querySelector("#spinner").remove();
+	} else {
+		return spinner;
+	}
+}
