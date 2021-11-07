4c7f582edbc3f3bccae9c4218887ce4d6ef3efb8
Michael Sliwinski <michaelnozbe@users.noreply.github.com>
2021-11-07 15:22:58 +0100

    previous next in blog posts

--- js/scripts.js
+++ js/scripts.js
@@ -45,6 +45,7 @@ function getArticle (url, where, what = "add") {
 }
 
 //getting the url of the latest post in the specified tag
+// tag = which tag to get, where = which HTML element, what = add or replace
 function getPostTag(tag, where, what = "add") {
 	let mytag = false;
 	let counter = 0; //to catch it only once
@@ -53,7 +54,8 @@ function getPostTag(tag, where, what = "add") {
 	.then((responseText) => {
 		let posts = JSON.parse(responseText, function(key, value) {
 			if (key == 'tags') {
-				if (value == tag){
+				let tags = value;
+				if (tags.indexOf(tag)>=0){ //if it contains a tag we seek
 					if (!counter) mytag = true;
 				}
 			}
@@ -62,6 +64,65 @@ function getPostTag(tag, where, what = "add") {
 					getArticle (value, where, what);
 					counter++;
 					mytag = false;
+				}
+			}
+		});
+	})
+	.catch((error) => {
+		console.error(error)
+	})
+}
+
+//posting previous and next titles and urls in the div "#prevnext"
+function postPrevNext (title='',slug='',type='next') {
+	let link = document.createElement('a');
+	let titleNew = (type == 'next') ?  title + ' »' : '« ' + title;
+	if (title) {
+		link.setAttribute('href', URL + slug);
+		link.innerHTML = goodTitle (titleNew);
+	}
+	if (type == 'next') {
+		link.setAttribute('class','next');
+		document.querySelector('#prevnext').append(link);
+	} else {
+		document.querySelector('#prevnext').prepend(link);
+	}
+}
+
+//getting previous (older) and next post (newer) for the current post
+function getPrevNext(slug) {
+	// let's add "#prevnext to footer of the blog post"
+	let prevnext = document.createElement('div');
+	prevnext.setAttribute('id','prevnext');
+	document.querySelector('footer').prepend(prevnext);
+	//let's define vars and get on with it
+	let prevTitle = ''; //title of previous post
+	let prevSlug = ''; //slug of previous post
+	let nextTitle = ''; //title of next post
+	let nextSlug = ''; //slug of next post
+	let thisTitle = ''; //title of this slug
+	let tempTitle = ''; //temporary title
+	fetch(URL + POSTS)
+	.then((response) => response.text())
+	.then((responseText) => {
+		let posts = JSON.parse(responseText, function(key, value) {
+			if (key == 'title') { tempTitle = value; } //we get the title of the current item
+			if (key == 'emoji') { tempTitle = value + ' ' + tempTitle; } //we add emoji to it
+			if (key=='url') {
+				if (value == slug) { // we found it so we post the NEXT post
+					thisTitle = tempTitle;
+					return postPrevNext(nextTitle,nextSlug);
+				} else {
+					if (thisTitle) { //if we found THIS then we deal previous post, not next.
+						if (!prevTitle) {
+							prevTitle = tempTitle;
+							prevSlug = value;
+							return postPrevNext(prevTitle,prevSlug,'prev');
+						}
+					} else { //we don't have this title yet, so everything else is a potential next stuff
+						nextTitle = tempTitle;
+						nextSlug = value;
+					}
 				}
 			}
 		});
@@ -126,6 +187,13 @@ function showYouTube(input = false) {
 	//add page to browser history if it was from input
 	if (input) window.history.pushState('video','YouTube watcher - Michael.team','https://michael.team/yt/?yt=https://www.youtube.com/watch?v='+yt);
 	}
+}
+
+// converts our safe HTML titles from JSON to nomral titles
+function goodTitle (title) {
+	return title
+		.replace('&amp;amp;','&')
+		.replace('&#39;',"'");
 }
 
 // get a random integer from a set
