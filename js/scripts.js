d267c3ad4f2d13a8cf428bf5b0e3e74007e829fe
Michael Sliwinski <michaelnozbe@users.noreply.github.com>
2021-11-14 10:57:47 +0100

    fixing scripts - findtags

--- js/scripts.js
+++ js/scripts.js
@@ -54,9 +54,7 @@ function getPostTag(tag, where, what = "add") {
 	.then((responseText) => {
 		let posts = JSON.parse(responseText, function(key, value) {
 			if (key == 'tags') {
-				if (findTags(tag,value))
-				let tags = value;
-				if (tags.indexOf(tag)>=0){ //if it contains a tag we seek
+				if (findTags(tag,value)) { //if it contains a tag we seek
 					if (!counter) mytag = true;
 				}
 			}
