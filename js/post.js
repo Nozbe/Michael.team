9a2bcd00cf02561c68b381b4a87100d2c155ac1f
Michael Sliwinski <michaelnozbe@users.noreply.github.com>
2022-10-26 17:54:39 +0200

    fixes to share links to put newsletter at the bottom

--- js/post.js
+++ js/post.js
@@ -15,8 +15,8 @@ function getRelated(slug) {
 	if (lang == '/pl') related.innerHTML = '<h3>Podobne wpisy:</h3>';
 	if (lang == '/es') related.innerHTML = '<h3>Entradas relacionadas:</h3>';
 	related.style.visibility = "hidden"; //we hide it first, before we show all of the posts
-	//document.querySelector('footer').prepend(related); //we switch to "append" to put it below sharing links
-	document.querySelector('footer').append(related);
+	//document.querySelector('footer').prepend(related); //we switch to "append" to put it below sharing links - so instead of "footer" we append them to "sharelinks" div
+	document.querySelector('sharelinks').append(related);
 	let tempTag = '';
 	let slugTag = '';
 	let counter = 0; //count to 3 related posts
@@ -77,7 +77,7 @@ function getPrevNext(slug) {
 	let prevnext = document.createElement('div');
 	prevnext.setAttribute('id','prevnext');
 	//document.querySelector('footer').prepend(prevnext); //we switch to "appending" it
-	document.querySelector('footer').append(prevnext);
+	document.querySelector('sharelinks').append(prevnext);
 	//lang detect - if the fourth char is / then get the first three chars
 	let lang = (slug.slice(3,4) == '/') ? slug.slice(0,3) : '';
 	//let's define vars and get on with it
